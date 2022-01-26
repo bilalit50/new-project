0e366bd0cb50ce9dbf4f2e3ff729f7d7c32c91bf
bilalit50 <bilalit50@gmail.com>
2022-01-26 18:58:52 +0100

    my new changes

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function abc(){
+    return(
+        <div>
+            <h2>Hello ABout</h2>
+            <h3>Hello New About</h3>
+        </div>
+    )
+
+}
+export default abc;
