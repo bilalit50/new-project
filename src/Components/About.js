a49812608c24496c58df6dac852008363ff4e3cb
bilalit50 <bilalit50@gmail.com>
2022-01-26 19:02:02 +0100

    just create object with name

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function abc(){
+    const[name, setState] = setState({firstname:"", lastname:""});
     return(
         <div>
             <h2>Hello ABout</h2>
             <h3>Hello New About</h3>
+            <input type="text"/>
         </div>
     )
 
